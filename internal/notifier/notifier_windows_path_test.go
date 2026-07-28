a8b9eaf747683a6754a9939d7c61fb68c2ba3498
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-07-28 11:10:12 -0300

    test(notifier): require absolute delivery paths

---
+++ internal/notifier/notifier_windows_path_test.go
@@ -0,0 +1,39 @@
+//go:build windows
+
+package notifier
+
+import (
+	"path/filepath"
+	"testing"
+
+	"git.sr.ht/~jackmordaunt/go-toast/v2"
+)
+
+func TestWindowsNotifierNormalizesRelativeDestinationBeforeRegister(t *testing.T) {
+	var notification *toast.Notification
+	n, _ := newWindowsNotifierFixture(t, func(value *toast.Notification) error {
+		notification = value
+		return nil
+	})
+
+	relative := filepath.Join("relative", "file.txt")
+	n.deliver(FileEvent{Destination: relative, Category: "Documents"})
+	if notification == nil {
+		t.Fatal("relative destination was not delivered")
+	}
+	_, eventID, err := parseNotificationAction(notification.ActivationArguments)
+	if err != nil {
+		t.Fatal(err)
+	}
+	event, ok := n.registry.Claim(eventID)
+	if !ok {
+		t.Fatal("relative destination event was not registered")
+	}
+	want, err := filepath.Abs(relative)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.CurrentPath != want {
+		t.Fatalf("registered path = %q, want %q", event.CurrentPath, want)
+	}
+}
